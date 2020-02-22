bf5c99e57fd369839212ebefdaf218a8368300be
Jacoblyon <blewitt.3@gmail.com>
2020-02-22 17:10:53 -0500

    Fixing issue where users other than admin (blewitt.3) could not get transactions.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,54 +5,48 @@ class TransactionsController < ActionController::API
     user_token = UserToken.find_by(token: params["userToken"])
     user_id = user_token.nil? ? nil : user_token.user_id
 
-    plaid_tokens = UserToken.where(user_id: user_id, token_type: "plaid_token").map {|t| t.token}
+    plaid_tokens = UserToken.where(user_id: user_id, token_type: "plaid_token").map { |t| t.token }
     access_tokens = plaid_tokens.length > 0 ? plaid_tokens : nil
 
     all_accounts_transactions = []
     date = params["date"] == "" ? user_token.user.last_viewed.split('/') : params["date"].split('/')
-    # TODO Remove this if/else buxfer stuff when plaid gets their act together and starts working with cap one.
-    if params["transactions"].nil?
-      x = 0
+    begin
+      unless access_tokens.nil?
+        plaid_env = Rails.application.config.plaid_env
+        client_id = Rails.application.config.client_id
+        secret = Rails.application.config.secret
+        public_key = Rails.application.config.public_key
+        client = Plaid::Client.new(env: plaid_env,
+                                   client_id: client_id,
+                                   secret: secret,
+                                   public_key: public_key)
 
-    else
-      begin
-        unless access_tokens.nil?
-          plaid_env = Rails.application.config.plaid_env
-          client_id = Rails.application.config.client_id
-          secret = Rails.application.config.secret
-          public_key = Rails.application.config.public_key
-          client = Plaid::Client.new(env: plaid_env,
-                                     client_id: client_id,
-                                     secret: secret,
-                                     public_key: public_key)
+        start_date = Date.new(date[0].to_i, date[1].to_i, 1).strftime('%Y-%m-%d')
+        end_date = Date.new(date[0].to_i, date[1].to_i, -1).strftime('%Y-%m-%d')
 
-          start_date = Date.new(date[0].to_i, date[1].to_i, 1).strftime('%Y-%m-%d')
-          end_date = Date.new(date[0].to_i, date[1].to_i, -1).strftime('%Y-%m-%d')
-
-          access_tokens.each do |access_token|
-            transaction_response = client.transactions.get(access_token, start_date, end_date)
-            transactions = transaction_response.transactions
-            # the transactions in the response are paginated, so make multiple calls while increasing the offset to retrieve all transactions
-            while transactions.length < transaction_response['total_transactions']
-              transaction_response = client.transactions.get(access_token, start_date, end_date, offset: transactions.length)
-              transactions += transaction_response.transactions
-            end
-            all_accounts_transactions += transactions
+        access_tokens.each do |access_token|
+          transaction_response = client.transactions.get(access_token, start_date, end_date)
+          transactions = transaction_response.transactions
+          # the transactions in the response are paginated, so make multiple calls while increasing the offset to retrieve all transactions
+          while transactions.length < transaction_response['total_transactions']
+            transaction_response = client.transactions.get(access_token, start_date, end_date, offset: transactions.length)
+            transactions += transaction_response.transactions
           end
-          all_accounts_transactions = transform_plaid_transactions(all_accounts_transactions)
+          all_accounts_transactions += transactions
+        end
+        all_accounts_transactions = transform_plaid_transactions(all_accounts_transactions)
 
-          # user = User.find_by(id: user_id)
-          all_accounts_transactions.each do |t|
-            unless Transaction.find_by(transaction_id: t[:id]) && user_id
-              transaction = Transaction.new(transaction_id: t[:id], user_id: user_id, description: t[:description], charge: t[:charge], date: t[:date], hidden: false, edited: false)
-              transaction.save
-            end
+        # user = User.find_by(id: user_id)
+        all_accounts_transactions.each do |t|
+          unless Transaction.find_by(transaction_id: t[:id]) && user_id
+            transaction = Transaction.new(transaction_id: t[:id], user_id: user_id, description: t[:description], charge: t[:charge], date: t[:date], hidden: false, edited: false)
+            transaction.save
           end
         end
-        render json: {transactions: all_accounts_transactions, date: date}.to_json
-      rescue
-        render json: {message: $!.error_code, status: 500, token: user_token.token}.to_json
       end
+      render json: {transactions: all_accounts_transactions, date: date}.to_json
+    rescue
+      render json: {message: $!.error_code, status: 500, token: user_token.token}.to_json
     end
   end
 
@@ -69,21 +63,21 @@ class TransactionsController < ActionController::API
     # Paginate through transaction to get every transaction for the current month.
     while current_month
       response = getResponse("transactions?token=#{token}&page=#{page}")
-      response['transactions'].each { |transaction|[0]
-        # Transactions from Buxfer have a leading 0 dates from the front end do not. Needed to account for this.
-        d = transaction['normalizedDate'].split('-')
-        transaction_month = d[1][0] == '0' ? d[1][1] : d[1]
-        transaction_year = d[0]
-        if (transaction_year.to_i < date[0].to_i) || (transaction_year.to_i == date[0].to_i && transaction_month.to_i < date[1].to_i)
-          # Stop looping if you are in last year or in last month.
-          current_month = false
-        elsif transaction_month != date[1]
-          # Next if you are in the wrong month
-          next
-        else
-          # Saves only the wanted month
-          all_accounts_transactions.push(transaction)
-        end
+      response['transactions'].each { |transaction| [0]
+      # Transactions from Buxfer have a leading 0 dates from the front end do not. Needed to account for this.
+      d = transaction['normalizedDate'].split('-')
+      transaction_month = d[1][0] == '0' ? d[1][1] : d[1]
+      transaction_year = d[0]
+      if (transaction_year.to_i < date[0].to_i) || (transaction_year.to_i == date[0].to_i && transaction_month.to_i < date[1].to_i)
+        # Stop looping if you are in last year or in last month.
+        current_month = false
+      elsif transaction_month != date[1]
+        # Next if you are in the wrong month
+        next
+      else
+        # Saves only the wanted month
+        all_accounts_transactions.push(transaction)
+      end
       }
       page += 1
     end
@@ -135,7 +129,7 @@ class TransactionsController < ActionController::API
   end
 
   def transform_plaid_transactions(transactions)
-    edited_transaction_ids = Transaction.where(edited: true).to_a.map {|e| e.transaction_id}
+    edited_transaction_ids = Transaction.where(edited: true).to_a.map { |e| e.transaction_id }
     transactions.map do |t|
       # Check to see if this transaction has been edited and stored in our Db. If so look it up and swap t with it.
       if edited_transaction_ids.include?(t.transaction_id)
@@ -150,7 +144,7 @@ class TransactionsController < ActionController::API
   end
 
   def transform_buxfer_transactions(transactions)
-    edited_transaction_ids = Transaction.where(edited: true).to_a.map {|e| e.transaction_id}
+    edited_transaction_ids = Transaction.where(edited: true).to_a.map { |e| e.transaction_id }
     transactions.map do |t|
       # Check to see if this transaction has been edited and stored in our Db. If so look it up and swap t with it.
       if edited_transaction_ids.include?(t["id"].to_s)
